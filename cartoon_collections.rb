d185c09e89b564609b93595d5af98fb4419db002
celestesmthomas <celestesmthomas@gmail.com>
2020-04-23 13:33:11 -0700

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,48 +1,133 @@
+require 'pry'
+
 def roll_call_dwarves(array)
-  array.each_with_index do |name, counter|
-  puts "#{counter +1}. #{name}"
+array.each_with_index  do |dwarf, index| 
+  puts "#{index+1}. #{dwarf}"
 end
 end
 
+def summon_captain_planet(array)
+ new_array = array.collect do |name|
+  name.capitalize << "!"
+ end
+ new_array
+end
 
-
-
-
-
-
-def summon_captain_planet(planteer_calls)
-  planteer_calls.collect do |name| 
-	name.capitalize + "!"
+def long_planeteer_calls(array)
+  array.any? do |name|
+    name.length >= 5
+end
   end
-end
+ 
 
-
-
-
-
-
-
-def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? do |call| call.length > 4
-  end
-end
-
-
-
-
-
-
-
-
-
-def find_the_cheese(snacks)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  snack = nil
-  snacks.each do |food| 
-    cheese_types.include?(food) 
-    if cheese_types.include?(food)
-      snack = food
+  def find_the_cheese(array)
+    cheeses = ["cheddar", "gouda", "thyme"]
+    i = 0
+    array.find do |element| element == cheeses[i]
+      i +=1
     end
   end
-  snack
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def roll_call_dwarves(array)
+#   array.each_with_index do |name, counter|
+#   puts "#{counter +1}. #{name}"
+# end
+# end
+
+
+
+
+
+
+
+# def summon_captain_planet(planteer_calls)
+#   planteer_calls.collect do |name| 
+# 	name.capitalize + "!"
+#   end
+# end
+
+
+
+
+
+
+
+# def long_planeteer_calls(planeteer_calls)
+#   planeteer_calls.any? do |call| call.length > 4
+#   end
+# end
+
+
+
+
+
+
+
+
+
+# def find_the_cheese(snacks)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   snack = nil
+#   snacks.each do |food| 
+#     cheese_types.include?(food) 
+#     if cheese_types.include?(food)
+#       snack = food
+#     end
+#   end
+#   snack
+# end
